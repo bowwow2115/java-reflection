9a7f7d3d3ff44b702b746f8f13117e0899e6edc0
Michael Pogrebinsky <smichpog2@gmail.com>
2020-04-15 08:46:43 -0700

    Ready implementation

---
+++ src/init/Main.java
@@ -0,0 +1,41 @@
+package init;
+
+import game.Game;
+import game.internal.TicTacToeGame;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
+
+        Game game = createObjectRecursively(TicTacToeGame.class);
+        game.startGame();
+    }
+
+    public static <T> T createObjectRecursively(Class<T> clazz) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
+        Constructor<?> constructor = getFirstPublicConstructor(clazz);
+
+        List<Object> constructorArguments = new ArrayList<>();
+
+        for (Class<?> argumentType : constructor.getParameterTypes()) {
+            Object argumentValue = createObjectRecursively(argumentType);
+            constructorArguments.add(argumentValue);
+        }
+
+        constructor.setAccessible(true);
+        return (T) constructor.newInstance(constructorArguments.toArray());
+    }
+
+    private static Constructor<?> getFirstPublicConstructor(Class<?> clazz) throws NoSuchMethodException {
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        if ( constructors.length == 0) {
+            throw new IllegalStateException(String.format("No constructor has been found for class %s", clazz));
+        }
+
+        return constructors[0];
+    }
+}
